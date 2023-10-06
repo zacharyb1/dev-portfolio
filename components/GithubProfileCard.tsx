83d3c8f774ac267891ab780fe15201b3ed4b05e8
ZacharyB1 <zachary.burda@gmail.com>
2023-10-06 11:55:05 +0300

    photo delited

--- components/GithubProfileCard.tsx
+++ components/GithubProfileCard.tsx
@@ -11,7 +11,7 @@ const GithubProfileCard = ({ avatar_url, bio, location }: GithubUserType) => {
           <Row className="">
             <Col className="order-lg-2" lg="4">
               <img
-                src={avatar_url}
+                //src={avatar_url}
                 style={{ width: '200px' }}
                 alt=""
                 className="rounded-circle img-center img-fluid shadow shadow-lg--hover mb-4"
@@ -26,7 +26,7 @@ const GithubProfileCard = ({ avatar_url, bio, location }: GithubUserType) => {
               <p className="text-white mt-3">{bio}</p>
               <div className="my-3 icon-shape bg-gradient-white shadow rounded text-info">
                 <i className="ni ni-pin-3 text-info mr-2" />
-                {location}
+                {"Helsinki, Finland"}
               </div>
               <SocialLinks />
             </Col>
